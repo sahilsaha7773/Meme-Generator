95868913a9e9cf0b9f9305c5a3f5f5cc93db6056
Sahil Saha <35343652+sahilsaha7773@users.noreply.github.com>
2020-06-10 10:33:16 +0530

    Updates

---
+++ precache-manifest.8f7146985f136f1f3a5df44e2b78ae83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16f60ff7f32d4654b8d0e3aefba6fba",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "13b1237dbaeb5b413912",
+    "url": "/Meme-Generator/static/js/2.070775ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-Generator/static/js/2.070775ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde54fa0c477db74f60c",
+    "url": "/Meme-Generator/static/js/main.b90dc0f9.chunk.js"
+  },
+  {
+    "revision": "c29cdf5fa9735770f06d",
+    "url": "/Meme-Generator/static/js/runtime-main.d8bc49ea.js"
+  }
+]);
